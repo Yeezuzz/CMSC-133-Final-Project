dfc4fa1aaa616283258cc4ae46f5d0f9f109b8f6
Yeezuzz <jasonzr0918@gmail.com>
2025-12-17 21:52:26 -0500

    Update beamWeapon.java
    Full implementation. Need to clean up unused parameters.

--- beamWeapon.java
+++ beamWeapon.java
@@ -1,18 +1,50 @@
-public class beamWeapon implements weapon {
+package app;
 
-  private String name;
-  private double chargeTime;
-  private int damage;
-  private int roomsHit;
-  
-  public beamWeapon(String name, double chargeTime, int damage, int roomsHit) {
-    this.name = name;
-    this.chargeTime = chargeTime;
-    this.damage = damage;
-    this.roomsHit = roomsHit;
-  }
+public class BeamWeapon implements WeaponInt {
+	private String name;
+	private int chargeTime;
+	private int damage;
+	private int shieldPenetration;
+	private int roomsHit;
+	  
+	public BeamWeapon(String name, int chargeTime, int damage, int shieldPenetration, int roomsHit) {
+	   this.name = name;
+	   this.chargeTime = chargeTime;
+	   this.damage = damage;
+	   this.shieldPenetration = shieldPenetration;
+	   this.roomsHit = roomsHit;
+	}
 
-  public int fire() {
-  }
-  
+	 
+	 public int fire() {
+		 chargeTime = 4;
+		 return damage * roomsHit;
+	 }
+	 
+	 public void charge() {
+		 if (chargeTime > 0) chargeTime--;
+	 }
+	 
+	 public void deCharge() {
+		 if (chargeTime < 4) chargeTime++;
+	 }
+	 
+	 public String getStatus() {
+		 if (chargeTime > 1) return "is not ready to fire"; 
+		 if (chargeTime > 0) return "will be ready to fire next turn";
+		 return "is ready to fire";
+	 }
+	 
+	 public String getName() {
+		 return name;
+	 }
+	 
+	 public int getChargeTime() {
+		 return chargeTime;
+	 }
+	 
+	 public void setChargeTime(int chargeTime) {
+		 this.chargeTime = chargeTime;
+	 }
+	 
 }
