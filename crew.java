1e7ced594a7b89b5cafb8fc11bf86eafcceb7e65
Yeezuzz <jasonzr0918@gmail.com>
2025-12-17 21:54:38 -0500

    Update crew.java
    Full implementation. Probably the least changed class. Crew don't play as much of an impact in my version due to the text based nature.

--- crew.java
+++ crew.java
@@ -1,29 +1,40 @@
+package app;
+
 public class Crew {
+	private String name;
+	  private int health;
+	  private String position;
+	  
+	  public Crew(String name, int health, String position) {
+	    this.name = name;
+	    this.health = health;
+	    this.position = position;
+	  }
+	  
+	  public Crew() {
+	    this("Crew Member", 100, "Engines");
+	  }
 
-  private String name;
-  private int health;
-  private String position
-  
-  public Crew(String name, int health, String position) {
-    this.name = name;
-    this.health = health;
-    this.position = position;
-  }
-  
-  public Crew() {
-    this("Crew Member", 100, "Piloting");
-  }
-
-  /*
-  public String move(String room) {
-    Implement after Ship has been built
-  }
-
-  public int repairSystem() {
-  
-  }
-  */
-
-  
-  
+	  
+	  public String move(String room) {
+	  	if (room.equals("shields") || room.equals("weapons") || room.equals("engines") || room.equals("hull")) {
+	  		position = room;
+	  		return name + " moved to " + room;
+	  	} else {
+	  		return "Invalid room specified.";
+	  	}
+	  }
+	  
+	  public String getName() {
+		  return name;
+	  }
+	  
+	  public String getPosition() {
+		  return position;
+	  }
+	  
+	  public void setPosition(String position) {
+		  this.position = position;
+	  }
+	  
 }
