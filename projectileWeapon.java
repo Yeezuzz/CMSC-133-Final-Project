3dc0b27ede38ff1c8d7d2ea3751bdf8c9b2f7808
Yeezuzz <jasonzr0918@gmail.com>
2025-12-17 21:51:48 -0500

    Update projectileWeapon.java
    Full implementation including firing, charging, and properties. Need to implement missile weapons properly.

--- projectileWeapon.java
+++ projectileWeapon.java
@@ -1,45 +1,75 @@
-public class projectileWeapon {
+package app;
 
-  private String name;
-  private double chargeTime;
-  private int damage;
-  private int shotsFired;
-  private boolean ignoresShield;
-  private int stunTime;
-
-  public projectileWeapon(String name, double chargeTime, int damage, int shotsFired, boolean ignoresShield, int stunTime) {
-    this.name = name;
-    this.chargeTime = chargeTime;
-    this.damage = damage;
-    this.shotsFired = shotsFired;
-    this.ignoresShield = ignoresShield;
-    this.stunTime = stunTime;
-  }
-
-  public projectileWeapon() {
-    this("Burst Laser Mk. II", 3.0, 1, 3, false, 0);
-  }
-
-  private int fire() {
-    if (name.contains("Missile") || name.contains("Beam")) {
-      chargeTime = 4.0
-    } else {
-      chargeTime = 3.0;
-    }
-    return damage * shotsFired;
-  }
-
-  private boolean charge() {
-    if (chargeTime > 0) {
-      chargeTime--;
-      return true;
-    } else {
-      return false;
-    }
-  }
-
-  public double getChargeTime() {
-    return chargeTime;
-  }
-
-}
+public class ProjectileWeapon implements WeaponInt {
+	private String name;
+	private int chargeTime;
+	private int damage;
+	private int shotsFired;
+	private boolean ignoresShield;
+	private int stunTime;
+	
+	public ProjectileWeapon(String name, int chargeTime, int damage, int shotsFired, boolean ignoresShield, int stunTime) {
+	  this.name = name;
+	  this.chargeTime = chargeTime;
+	  this.damage = damage;
+	  this.shotsFired = shotsFired;
+	  this.ignoresShield = ignoresShield;
+	  this.stunTime = stunTime;
+	  }
+	
+	  public ProjectileWeapon() {
+	    this("Burst Laser Mk. II", 3, 1, 3, false, 0);
+	  }
+	
+	  public int fire() {
+		  if (name.contains("Missile")) {
+			  chargeTime = 4;
+		  } else {
+			  chargeTime = 3;
+		  }
+		  return damage * shotsFired;
+	  }
+	  
+	  public int fire(int shots) {
+	    if (name.contains("Missile")) {
+	      chargeTime = 4;
+	    } else if (name.contains("Ion")) {
+	    	chargeTime = 3;
+	    	return stunTime * shots;
+	    }else {
+	      chargeTime = 3;
+	    }
+	    return damage * shots;
+	  }
+	
+	  public void charge() {
+	    if (chargeTime > 0) chargeTime--;
+	  }
+	  
+	  public void deCharge() {
+		  if (name.contains("Missile")) {
+			  if (chargeTime < 4) chargeTime++;
+		  } else {
+			  if (chargeTime < 3) chargeTime++;
+		  }
+	  }
+	
+	  public String getStatus() {
+	    if (chargeTime > 1) return "is not ready to fire"; 
+	    if (chargeTime > 0) return "will be ready to fire next turn";
+	    return "is ready to fire";
+	  }
+	  
+	  public String getName() {
+		  return name;
+	  }
+	  
+	  public int getChargeTime() {
+		  return chargeTime;
+	  }
+	  
+	  public int getShotsFired() {
+		  return shotsFired;
+	  }
+	  
+	}
